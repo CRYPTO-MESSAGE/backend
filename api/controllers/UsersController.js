dc102d9f8722795feeaffd31793f540762ef67cb
Florian Cellier <chadyred@gmail.com>
2017-05-18 16:49:11 +0200

    Add strength password check

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -10,15 +10,23 @@ module.exports = {
     if (req.body.password !== req.body.confirmPassword) {
       return res.json(401, {err: 'Password doesn\'t match, What a shame!'});
     }
-    Users.create(req.body).exec(function (err, user) {
-      if (err) {
-        return res.json(err.status, {err: err});
-      }
-      // If user created successfuly we return user and token as response
-      if (user) {
-        // NOTE: payload is { id: user.id}
-        res.json(200, {user: user, token: jwToken.issue({id: user.id})});
-      }
-    });
+    Users.checkStrength(req.body.password, function (err, valid) {
+        if (!valid) {
+          return res.json(401, {err: err});
+        }
+        else {
+          Users.create(req.body).exec(function (err, user) {
+            if (err) {
+              return res.json(err.status, {err: err});
+            }
+            // If user created successfuly we return user and token as response
+            if (user) {
+              // NOTE: payload is { id: user.id}
+              res.json(200, { user: user, token: jwToken.issue( { id: user.id } ) } );
+            }
+          });
+        }
+      }); //Strength
+    
   }
 };
