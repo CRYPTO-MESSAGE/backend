980cc263d312fa20e5c8ae37f43f9eca6f2afa55
Florian Cellier <chadyred@gmail.com>
2017-05-19 10:28:29 +0200

    Debug api sever

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -10,24 +10,29 @@ module.exports = {
     var username = req.param('username');
     var password = req.param('password');
 
-    console.log("User /auth index action")
+    sails.log("User /auth index action")
     if (!username || !password) {
+      sails.log("username and password required")
       return res.json(401, {err: 'username and password required'});
     }
 
     Users.findOne({username: username}, function (err, user) {
       if (!user) {
+        sails.log("invalid username or password")
         return res.json(401, {err: 'invalid username or password'});
       }
 
       Users.comparePassword(password, user, function (err, valid) {
         if (err) {
+          sails.log("forbidden")
           return res.json(403, {err: 'forbidden'});
         }
 
         if (!valid) {
+          sails.log("Secondtime :after bcrypt check invalid username or password")
           return res.json(401, {err: 'invalid username or password'});
         } else {
+          sails.log("Successs")
           res.json({
             user: user,
             token: jwToken.issue({id : user.id })
